3f617a57889217fe54e3a262ec6d59e4b7b28f23
hamzasolicode <91888808+hamzasolicode@users.noreply.github.com>
2022-03-02 14:35:36 +0100

    Update drawSQL-mysql-export-2022-03-02 .sql

--- drawSQL-mysql-export-2022-03-02 .sql
+++ drawSQL-mysql-export-2022-03-02 .sql
@@ -1,51 +1,32 @@
-CREATE TABLE `Produit`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `Titre` VARCHAR(255) NOT NULL,
-    `prix` INT NOT NULL,
-    `stack` INT NOT NULL,
-    `description` TEXT NOT NULL
+CREATE TABLE "Produit"(
+    "Produit id" INT NOT NULL,
+    "titre" NVARCHAR(255) NOT NULL,
+    "prix" INT NOT NULL,
+    "stack" INT NOT NULL
 );
+
 ALTER TABLE
-    `Produit` ADD PRIMARY KEY `produit_id_primary`(`id`);
-CREATE TABLE `num-produit`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `produit-id` INT NOT NULL,
-    `produit_num` INT NOT NULL
+    "Produit" ADD CONSTRAINT "produit_produit id_primary" PRIMARY KEY("Produit id");
+CREATE TABLE "Ordre"(
+    "Reference" INT NOT NULL,
+    "date" DATE NOT NULL,
+    "adress-order" INT NOT NULL
 );
+
 ALTER TABLE
-    `num-produit` ADD PRIMARY KEY `num_produit_id_primary`(`id`);
-CREATE TABLE `Ordre`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `produit_num_id` INT NOT NULL,
-    `date` DATE NOT NULL,
-    `adress_ordre` TEXT NOT NULL,
-    `ordre_id_num` INT NOT NULL
+    "Ordre" ADD CONSTRAINT "ordre_reference_primary" PRIMARY KEY("Reference");
+CREATE TABLE "Client"(
+    "Client Id" INT NOT NULL,
+    "Nom" INT NOT NULL,
+    "Prénom" INT NOT NULL,
+    "Tel" INT NOT NULL,
+    "Adress-client" INT NOT NULL,
+    "email" INT NOT NULL,
+    "password" INT NOT NULL
 );
+
 ALTER TABLE
-    `Ordre` ADD PRIMARY KEY `ordre_id_primary`(`id`);
-CREATE TABLE `Ordre_num`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `num_client` INT NOT NULL,
-    `num_ordre` INT NOT NULL
-);
+    "Client" ADD CONSTRAINT "client_client id_primary" PRIMARY KEY("Client Id");
+CREATE TABLE "Quantité details"("Ordre Quantitée" INT NOT NULL);
 ALTER TABLE
-    `Ordre_num` ADD PRIMARY KEY `ordre_num_id_primary`(`id`);
-CREATE TABLE `Client`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `Nom` VARCHAR(255) NOT NULL,
-    `Prénom` VARCHAR(255) NOT NULL,
-    `Tel` INT NOT NULL,
-    `Adress_client` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `Password` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `Client` ADD PRIMARY KEY `client_id_primary`(`id`);
-ALTER TABLE
-    `Produit` ADD CONSTRAINT `produit_titre_foreign` FOREIGN KEY(`Titre`) REFERENCES `num-produit`(`id`);
-ALTER TABLE
-    `num-produit` ADD CONSTRAINT `num_produit_produit_id_foreign` FOREIGN KEY(`produit-id`) REFERENCES `Ordre`(`id`);
-ALTER TABLE
-    `Ordre` ADD CONSTRAINT `ordre_produit_num_id_foreign` FOREIGN KEY(`produit_num_id`) REFERENCES `Ordre_num`(`id`);
-ALTER TABLE
-    `Client` ADD CONSTRAINT `client_prénom_foreign` FOREIGN KEY(`Prénom`) REFERENCES `Ordre_num`(`id`);
+    "Client" ADD CONSTRAINT "client_nom_foreign" FOREIGN KEY("Nom") REFERENCES "Ordre"("Reference");
